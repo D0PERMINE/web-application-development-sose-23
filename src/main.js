047075be012c2801e0bf55a4c48546b4a41cdb8d
DOPERMINE <dopermine@web.de>
2023-05-20 01:55:29 +0200

    main screen shows when admina or normalo(without add button) logs in

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("submit-button");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+const loginScreen = document.getElementById("login-screen");
+const mainScreen = document.getElementById("main-screen");
+const addButton = document.getElementById("add-button");
+
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     console.log("OKKK: " + loginForm.username.value);
@@ -11,11 +15,19 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "admina" && password === "admina") {
         alert("You have successfully logged in as admina.");
-        location.reload();
+        // location.reload();
+        loginScreen.style.display = "none";
+        mainScreen.style.display = "block";
     } else if (username === "normalo" && password === "normalo") {
         alert("You have successfully logged in as normalo.");
-        location.reload();
-    }else {
+        // location.reload();
+        loginScreen.style.display = "none";
+        mainScreen.style.display = "block";
+        addButton.style.display = "none";
+
+    } else {
         loginErrorMsg.style.opacity = 1;
+        // loginErrorMsg.style.display = "block";
+        // loginErrorMsg.style.visibility = "visible";
     }
 })
